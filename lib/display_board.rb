a91700d4031b045047421c37e49b37aa4bb204fc
Mark Drehmann <markdrehmann@gmail.com>
2019-12-14 22:23:33 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,13 +1,14 @@
 board = [" "," "," "," "," "," "," "," "," "]
 
 def display_board
+  board = [" "," "," "," "," "," "," "," "," "]
+
+
   puts " #{board(0)} | #{board(1)} | #{board(2)}
   -----------
   #{board(3)} | #{board(4)} | #{board(5)}
      -----------
      #{board(6)} | #{board(7)} | #{board(8)} "
-
-     board = [" "," "," "," "," "," "," "," "," "]
 end
 
 puts display_board
